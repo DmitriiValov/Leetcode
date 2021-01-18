7ed74f88b7886fe7cd9b2fc8442ff0d19fe77fcf
Dmitry Valov <pt.dvalov@prosper.com>
2021-01-18 13:56:09 +0600

    Added solved problem - Roman to Integer

---
+++ src/main/java/_13.java
@@ -0,0 +1,115 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <a href="https://leetcode.com/problems/roman-to-integer/">13. Roman to Integer</a>
+ */
+
+public class _13 {
+
+    public static class Solution1 {
+
+        public int romanToInt(String s) {
+            int result = 0;
+            for (int i = 0; i < s.length(); ++i) {
+                if (i + 1 < s.length()) {
+                    if (s.charAt(i) == 'I') {
+                        if (s.charAt(i + 1) == 'V') {
+                            result += 4;
+                            ++i;
+                            continue;
+                        }
+                        else if (s.charAt(i + 1) == 'X') {
+                            result += 9;
+                            ++i;
+                            continue;
+                        }
+                    }
+                    else if (s.charAt(i) == 'X') {
+                        if (s.charAt(i + 1) == 'L') {
+                            result += 40;
+                            ++i;
+                            continue;
+                        }
+                        else if (s.charAt(i + 1) == 'C') {
+                            result += 90;
+                            ++i;
+                            continue;
+                        }
+                    }
+                    else if (s.charAt(i) == 'C') {
+                        if (s.charAt(i + 1) == 'D') {
+                            result += 400;
+                            ++i;
+                            continue;
+                        }
+                        else if (s.charAt(i + 1) == 'M') {
+                            result += 900;
+                            ++i;
+                            continue;
+                        }
+                    }
+                }
+                switch (s.charAt(i)) {
+                    case 'I': {
+                        result += 1;
+                        break;
+                    }
+                    case 'V': {
+                        result += 5;
+                        break;
+                    }
+                    case 'X': {
+                        result += 10;
+                        break;
+                    }
+                    case 'L': {
+                        result += 50;
+                        break;
+                    }
+                    case 'C': {
+                        result += 100;
+                        break;
+                    }
+                    case 'D': {
+                        result += 500;
+                        break;
+                    }
+                    case 'M': {
+                        result += 1000;
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+
+    public static class Solution2 {
+
+        public int romanToInt(String s) {
+            int result = 0;
+            Map<Character, Integer> abc = new HashMap<>();
+
+            abc.put('I', 1);
+            abc.put('V', 5);
+            abc.put('X', 10);
+            abc.put('L', 50);
+            abc.put('C', 100);
+            abc.put('D', 500);
+            abc.put('M', 1000);
+
+            for (int i = 0; i < s.length(); ++i) {
+                int value = abc.get(s.charAt(i));
+                if (i > 0) {
+                    int prevValue = abc.get(s.charAt(i - 1));
+                    if (value > prevValue) {
+                        value -= 2 * prevValue;
+                    }
+                }
+                result += value;
+            }
+            return result;
+        }
+    }
+}
---
+++ src/test/java/_13_Test.java
@@ -0,0 +1,75 @@
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class _13_Test {
+    private static _13.Solution1 solution1;
+    private static _13.Solution1 solution2;
+
+    @BeforeClass
+    public static void setup() {
+        solution1 = new _13.Solution1();
+        solution2 = new _13.Solution1();
+    }
+
+    @Test
+    public void test1() {
+        String s = "III";
+        assertThat(solution1.romanToInt(s)).isEqualTo(3);
+    }
+
+    @Test
+    public void test2() {
+        String s = "IV";
+        assertThat(solution1.romanToInt(s)).isEqualTo(4);
+    }
+
+    @Test
+    public void test3() {
+        String s = "IX";
+        assertThat(solution1.romanToInt(s)).isEqualTo(9);
+    }
+
+    @Test
+    public void test4() {
+        String s = "LVIII";
+        assertThat(solution1.romanToInt(s)).isEqualTo(58);
+    }
+
+    @Test
+    public void test5() {
+        String s = "MCMXCIV";
+        assertThat(solution1.romanToInt(s)).isEqualTo(1994);
+    }
+
+    @Test
+    public void test21() {
+        String s = "III";
+        assertThat(solution2.romanToInt(s)).isEqualTo(3);
+    }
+
+    @Test
+    public void test22() {
+        String s = "IV";
+        assertThat(solution2.romanToInt(s)).isEqualTo(4);
+    }
+
+    @Test
+    public void test23() {
+        String s = "IX";
+        assertThat(solution2.romanToInt(s)).isEqualTo(9);
+    }
+
+    @Test
+    public void test24() {
+        String s = "LVIII";
+        assertThat(solution2.romanToInt(s)).isEqualTo(58);
+    }
+
+    @Test
+    public void test25() {
+        String s = "MCMXCIV";
+        assertThat(solution2.romanToInt(s)).isEqualTo(1994);
+    }
+}
