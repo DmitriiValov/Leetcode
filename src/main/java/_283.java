bff2a602ec1a3dde1a6f0fbf1847fc4a054bc13c
Dmitry Valov <pt.dvalov@prosper.com>
2021-02-06 07:37:52 +0600

    Added solved problems - Move Zeroes

--- src/main/java/_283.java
+++ src/main/java/_283.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+
+/**
+ * <a href="https://leetcode.com/problems/move-zeroes/">283. Move Zeroes</a>
+ */
 
 public class _283 {
+
+    public static class Solution {
+
+        public void moveZeroes(int[] nums) {
+            int zeroCount = 0;
+            for (int i = 0; i < nums.length; ++i) {
+                if (nums[i] == 0) {
+                    ++zeroCount;
+                }
+                else if (zeroCount > 0) {
+                    nums[i - zeroCount] = nums[i];
+                    nums[i] = 0;
+                }
+            }
+        }
+    }
 }
--- src/test/java/_283_Test.java
+++ src/test/java/_283_Test.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import org.junit.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class _283_Test {
+
+    private static final _283.Solution SOLUTION = new _283.Solution();
+
+    @Test
+    public void test1() {
+        int[] nums = {0,1,0,3,12};
+        int[] result = {1,3,12,0,0};
+        SOLUTION.moveZeroes(nums);
+        assertThat(nums).isEqualTo(result);
+    }
 }
