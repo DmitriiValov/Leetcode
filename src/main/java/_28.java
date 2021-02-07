495aee88bed35e919893226f8bb4cc034d209ab6
Dmitry Valov <pt.dvalov@prosper.com>
2021-02-07 19:30:47 +0600

    Added solved problems - Implement strStr()

--- src/main/java/_28.java
+++ src/main/java/_28.java
@@ -7,8 +7,21 @@ public class _28 {
 
     public static class Solution {
         public int strStr(String haystack, String needle) {
-            for (int i = 0; i < haystack.length(); ++i) {
+            if (needle.length() == 0) {
+                return 0;
+            }
+            char[] h = haystack.toCharArray();
+            char[] n = needle.toCharArray();
 
+            for (int i = 0; i < h.length - n.length + 1; ++i) {
+                for (int j = 0; j < n.length; ++j) {
+                    if (h[i + j] != n[j]) {
+                        break;
+                    }
+                    if (j == n.length - 1) {
+                        return i;
+                    }
+                }
             }
             return -1;
         }
