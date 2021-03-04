12fc83d5c16ae9dc5aeba84b4c02e3969c8022da
Dmitry Valov <d_valov@mail.ru>
2021-03-04 08:54:54 +0600

    Added solved problem -  Missing Number

--- src/main/java/_268.java
+++ src/main/java/_268.java
@@ -8,12 +8,11 @@ public class _268 {
     public static class Solution {
         public int missingNumber(int[] nums) {
             long result = 0;
-            long summ = 0;
             for (int i = 0; i < nums.length; ++i) {
-                summ += nums[i];
+                result -= nums[i];
                 result += i;
             }
-            return (int)(result + nums.length - summ);
+            return (int)(result + nums.length);
         }
     }
 }
